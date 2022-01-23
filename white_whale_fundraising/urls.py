e35c133729a56adbb4fb036e6e070c49e361fbc0
James-VT <jd_tanner@hotmail.co.uk>
2022-01-23 23:39:59 +0000

    Adds CSS and media folder to project

--- white_whale_fundraising/urls.py
+++ white_whale_fundraising/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# Import the below to allow Django to see the MEDIA_URL in settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
