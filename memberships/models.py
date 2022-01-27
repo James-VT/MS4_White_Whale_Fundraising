73e2139c664a949fbcc9dad558efecdd1de481c0
James-VT <jd_tanner@hotmail.co.uk>
2022-01-27 22:06:58 +0000

    Edit return paramater in memmbershiplevel class

--- memberships/models.py
+++ memberships/models.py
@@ -9,7 +9,7 @@ class MembershipLevel(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.friendly_name
+        return self.name
 
     def get_friendly_name(self):
         return self.friendly_name
