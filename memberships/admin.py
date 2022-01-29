d991b7f5e5a8439e979780bb1ccb7da86e824628
James-VT <jd_tanner@hotmail.co.uk>
2022-01-29 12:54:17 +0000

    Adds the membership model to membership/admin.py

--- memberships/admin.py
+++ memberships/admin.py
@@ -1,3 +1,6 @@
+""" Registers the membership models for the site admin """
 from django.contrib import admin
+from .models import MembershipLevel
 
 # Register your models here.
+admin.site.register(MembershipLevel)
--- memberships/models.py
+++ memberships/models.py
@@ -1,3 +1,5 @@
+""" Model for the membership choices """
+
 from django.db import models
 
 # Create your models here.
@@ -13,6 +15,10 @@ class MembershipLevel(models.Model):
         (SILVER, 'Silver Level'),
         (BRONZE, 'Bronze Level'),
     ]
+
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     # name = models.CharField(max_length-254)
     # friendly_name = models.CharField(max_length=254, null=True, blank=True)
