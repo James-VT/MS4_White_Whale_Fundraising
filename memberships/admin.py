60d0cb6fded68a9fced6a0d5a1a7d5727b9d7eb6
James-VT <jd_tanner@hotmail.co.uk>
2022-01-30 15:43:30 +0000

    Reworks the membership model and admin for it to get it working

--- memberships/admin.py
+++ memberships/admin.py
@@ -1,23 +1,23 @@
 """ Registers the membership models for the site admin """
 from django.contrib import admin
-from .models import MembershipLevel
+from .models import Membership
 
 # Register your models here.
 
 
-class MembershipAdmin(admin.ModelAdmin):
-    """ Gives the admin side a proper class for memberships """
-    # readonly_fields = ('membership_number', 'date', 'membership_price',
-    #                    'membership_level',)
+# class MembershipAdmin(admin.ModelAdmin):
+#     """ Gives the admin side a proper class for memberships """
+#     readonly_fields = ('membership_number', 'date', 'membership_price',
+#                        'membership_level',)
 
-    # fields = ('membership_number', 'date', 'membership_price',
-    #           'membership_profile', 'membership_level', 'full_name',
-    #           'email', 'phone_number', 'street_address1', 'street_address2',
-    #           'town_or_city', 'postcode', 'country',)
+#     fields = ('membership_number', 'date', 'membership_price',
+#               'membership_profile', 'membership_level', 'full_name',
+#               'email', 'phone_number', 'street_address1', 'street_address2',
+#               'town_or_city', 'postcode', 'country',)
 
-    list_display = ('sku', 'level', 'description', 'price',)
+#     list_display = ('sku', 'level', 'description', 'price',)
 
-    # ordering = ('sku',)
+#     ordering = ('sku',)
 
 
-admin.site.register(MembershipLevel, MembershipAdmin)
+admin.site.register(Membership)
--- memberships/models.py
+++ memberships/models.py
@@ -5,28 +5,43 @@ from django.db import models
 # Create your models here.
 
 
-class MembershipLevel(models.Model):
-    """Defines the level of a member's subscription"""
-    GOLD = 'GO'
-    SILVER = 'SI'
-    BRONZE = 'BR'
-    MEMBERSHIP_CHOICES = [
-        (GOLD, 'Gold Level'),
-        (SILVER, 'Silver Level'),
-        (BRONZE, 'Bronze Level'),
-    ]
-
-    level = models.CharField(max_length=6, choices=MEMBERSHIP_CHOICES,
-                             default=BRONZE)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+class Membership(models.Model):
+    """ Class for memberships """
+    name = models.CharField(max_length=10)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+# class MembershipLevel(models.Model):
+#     """Defines the level of a member's subscription"""
+#     GOLD = 'GO'
+#     SILVER = 'SI'
+#     BRONZE = 'BR'
+#     MEMBERSHIP_CHOICES = [
+#         (GOLD, 'Gold Level'),
+#         (SILVER, 'Silver Level'),
+#         (BRONZE, 'Bronze Level'),
+#     ]
+
+#     level = models.CharField(max_length=6, choices=MEMBERSHIP_CHOICES,
+#                              default=BRONZE, description = models.TextField(),
+#                              sku = models.CharField(max_length=254, null=True, blank=True,
+#                              price = models.DecimalField(max_digits=6, decimal_places=2)
+#     )
+
+#     def __str__(self):
+#         return self.level in {self.GOLD, self.SILVER, self.BRONZE}
+
+    # sku = models.CharField(max_length=254, null=True, blank=True)
+    # description = models.TextField()
+    # price = models.DecimalField(max_digits=6, decimal_places=2)
 
     # name = models.CharField(max_length-254)
     # friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    # def __str__(self):
-    #     return self.name
 
     # def get_friendly_name(self):
     #     return self.friendly_name
