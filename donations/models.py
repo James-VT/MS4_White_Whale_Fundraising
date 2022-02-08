fa98ac48a3d8fcc5f2e65492306bcf8695131b5f
James-VT <jd_tanner@hotmail.co.uk>
2022-02-08 15:35:01 +0000

    Creates initial class for donations

--- donations/models.py
+++ donations/models.py
@@ -1,3 +1,20 @@
+""" Models for the donation form """
 from django.db import models
 
-# Create your models here.
+
+class Donations(models.Model):
+    """ Class for handling donations """
+    donation_number = models.CharField(max_length=32,
+                                       null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
