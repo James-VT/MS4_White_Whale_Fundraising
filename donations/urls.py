c701239670d8c4db95210fadbdf0af93469b768a
James-VT <jd_tanner@hotmail.co.uk>
2022-02-11 08:17:18 +0000

    Added URLs and more views for the donation form

--- donations/urls.py
+++ donations/urls.py
@@ -1,6 +1,9 @@
+""" URLs for the donation form """
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.donation_form, name="donation_form"),
+    path('charge/', views.charge, name="charge"),
     path('donation_success/', views.success_msg, name="donation_success"),
 ]
--- donations/views.py
+++ donations/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+""" Views for our donation pages """
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import JsonResponse
+
+
+def donation_form(request):
+    """ Donation form page """
+    return render(request, 'donations/donation_form.html')
+
+
+def charge(request):
+    """ View for taking payment """
+    amount = 0
+    if request.method == 'POST':
+        print('Data:', request.POST)
+
+    return redirect(reverse('success', args=[amount]))
 
 
 def success_msg(request, args):
