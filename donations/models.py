14d8670e705f8d774ebdb804c72512f58d1708bf
James-VT <jd_tanner@hotmail.co.uk>
2022-02-15 21:03:01 +0000

    Add new form fields to donations/models.py

--- donations/models.py
+++ donations/models.py
@@ -8,10 +8,12 @@ class Donation(models.Model):
     """ Class for handling donations """
     donation_number = models.CharField(max_length=32,
                                        null=False, editable=False)
-    full_name = models.CharField(max_length=50, null=False, blank=False)
+    title = models.CharField(max_length=32, null=False, blank=False)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
