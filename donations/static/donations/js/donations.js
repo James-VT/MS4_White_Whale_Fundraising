e1279dcdd62ed0092aad756c715e778ca415ebc9
James-VT <jd_tanner@hotmail.co.uk>
2022-02-15 14:58:47 +0000

    Removes commented code and console log from donations.js

--- donations/static/donations/js/donations.js
+++ donations/static/donations/js/donations.js
@@ -1,13 +1,8 @@
 var amountCustom = document.getElementById('amount-custom');
 
-console.log(typeof amountCustom);
-
 amountCustom.addEventListener("click", uncheckRadio);
 
 function uncheckRadio() {
-    // var radio = document.querySelector('input[type=radio]:checked');
-    // radio.checked = false;
-    // }
 
     if (document.querySelector('input[type=radio]:checked')) {
         var radio = document.querySelector('input[type=radio]:checked');
