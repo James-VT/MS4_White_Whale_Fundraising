bde25350757af09bc56f4dadc8ea6167ba444a4b
James-VT <jd_tanner@hotmail.co.uk>
2022-02-18 12:45:36 +0000

    Edit userprofile model to allow superuser to re-register temporarily

--- profiles/models.py
+++ profiles/models.py
@@ -12,13 +12,13 @@ class UserProfile(models.Model):
     User profile model for storing default contact info
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    town_or_city = models.CharField(max_length=50, null=False, blank=False)
+    town_or_city = models.CharField(max_length=50, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=8, null=True, blank=True)
     country = CountryField(blank_label='Country *', null=True, blank=True)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
