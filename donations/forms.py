72c62d90a52d1dee5274231ce0e3ba13c0ec8b80
James-VT <jd_tanner@hotmail.co.uk>
2022-02-21 16:02:08 +0000

    Builds form for donations in forms.py

---
+++ donations/forms.py
@@ -0,0 +1,44 @@
+""" Donation form """
+from django import forms
+from .models import Donation
+
+
+class DonationForm(forms.ModelForm):
+    """ Donation form class """
+    class Meta:
+        """ Meta class for donation form """
+        model = Donation
+        fields = ('title', 'first_name', 'last_name',
+                  'email', 'phone_number', 'country',
+                  'postcode', 'town_or_city', 'street_address1',
+                  'street_address2', 'county', 'date', 'gift_aid',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'email_address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postcode',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County, State or Locality'
+        }
+
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholder[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].label = False
