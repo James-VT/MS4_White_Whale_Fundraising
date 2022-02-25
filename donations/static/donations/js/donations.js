430fa9a6872ce64d058997db74e43c6ee69994a2
James-VT <jd_tanner@hotmail.co.uk>
2022-02-25 22:46:49 +0000

    Add new gift aid and info sections to donation form, with js to reveal them

--- donations/static/donations/js/donations.js
+++ donations/static/donations/js/donations.js
@@ -1,5 +1,17 @@
 var amountCustom = document.getElementById('amount-custom');
 
+var giftAidExplanationButton = document.getElementById("gift-aid-info-button");
+
+var giftAidExplanationParagraph = document.getElementById("gift-aid-explanation");
+
+var detailsInfoButton = document.getElementById("details-info-button");
+
+var detailsInfo = document.getElementById("details-info");
+
+detailsInfoButton.addEventListener("click", showDetailsExplanation);
+
+giftAidExplanationButton.addEventListener("click", showGiftAidExplanation);
+
 amountCustom.addEventListener("click", uncheckRadio);
 
 function uncheckRadio() {
@@ -7,5 +19,23 @@ function uncheckRadio() {
     if (document.querySelector('input[type=radio]:checked')) {
         var radio = document.querySelector('input[type=radio]:checked');
         radio.checked = false;
+    }
+}
+
+function showGiftAidExplanation() {
+    
+    if (giftAidExplanationParagraph.style.display === "none") {
+        giftAidExplanationParagraph.style.display = "block";
+    } else {
+        giftAidExplanationParagraph.style.display = "none";
+    }
+}
+
+function showDetailsExplanation() {
+
+    if (detailsInfo.style.display === "none") {
+        detailsInfo.style.display = "block";
+    } else {
+        detailsInfo.style.display = "none";
     }
 }
