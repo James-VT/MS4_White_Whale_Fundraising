d39deaa675525b7f9ffd1459aaa6d2e9135174cb
James-VT <jd_tanner@hotmail.co.uk>
2022-02-26 21:24:49 +0000

    Edits JS for click events on donation form and removes the inline styling for same

--- donations/static/donations/js/donations.js
+++ donations/static/donations/js/donations.js
@@ -1,12 +1,12 @@
 var amountCustom = document.getElementById('amount-custom');
 
-var giftAidExplanationButton = document.getElementById("gift-aid-info-button");
+const giftAidExplanationButton = document.getElementById("gift-aid-info-button");
 
-var giftAidExplanationParagraph = document.getElementById("gift-aid-explanation");
+const giftAidExplanationParagraph = document.getElementById("gift-aid-explanation");
 
-var detailsInfoButton = document.getElementById("details-info-button");
+const detailsInfoButton = document.getElementById("details-info-button");
 
-var detailsInfo = document.getElementById("details-info");
+const detailsInfo = document.getElementById("details-info");
 
 detailsInfoButton.addEventListener("click", showDetailsExplanation);
 
@@ -23,19 +23,13 @@ function uncheckRadio() {
 }
 
 function showGiftAidExplanation() {
-    
-    if (giftAidExplanationParagraph.style.display === "none") {
-        giftAidExplanationParagraph.style.display = "block";
-    } else {
-        giftAidExplanationParagraph.style.display = "none";
-    }
+
+    giftAidExplanationParagraph.classList.toggle("gift-aid-explanation-class");
+
 }
 
 function showDetailsExplanation() {
+    
+    detailsInfo.classList.toggle("details-info-class");
 
-    if (detailsInfo.style.display === "none") {
-        detailsInfo.style.display = "block";
-    } else {
-        detailsInfo.style.display = "none";
-    }
 }
