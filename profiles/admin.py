288cc68c03a251a1aeb73a0a9b7ff70aa301e8f0
James-VT <jd_tanner@hotmail.co.uk>
2022-02-28 22:40:14 +0000

    Refactor profile admin code to be pep8 compliant

--- profiles/admin.py
+++ profiles/admin.py
@@ -5,9 +5,12 @@ from .models import UserProfile
 
 class ProfileAdmin(admin.ModelAdmin):
     """ Class for the admin of profiles """
-    fields = ('default_user', 'default_title', 'default_first_name', 'default_last_name',
-              'default_email', 'default_phone_number', 'default_country', 'default_postcode',
-              'default_town_or_city', 'default_street_address1', 'default_street_address2',
+    fields = ('default_user', 'default_title', 'default_first_name',
+              'default_last_name', 'default_email',
+              'default_phone_number', 'default_country',
+              'default_postcode',
+              'default_town_or_city', 'default_street_address1',
+              'default_street_address2',
               'default_county',)
 
 
