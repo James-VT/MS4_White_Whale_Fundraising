091d34ef90bf16790677b1edc6d9c4e915811cbd
James-VT <jd_tanner@hotmail.co.uk>
2022-03-06 14:25:39 +0000

    Add return to profile button to donation success page

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('donation_history/<donation_history>', views.donation_history, name='donation_history'),
 ]
